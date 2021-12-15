3735a8fe97dee137c93656a7118074a7adf20df0
王海涛 <859926750@qq.com>
2021-12-15 14:05:20 +0800

    登录模块完成

--- Command/user.go
+++ Command/user.go
@@ -1,18 +1,45 @@
 package Command
 
-//
-//import (
-//	"fmt"
-//	"github.com/gin-gonic/gin"
-//	"net/http"
-//)
-//
-//func Login(c *gin.Context) {
-//	name:=c.Param("name")
-//	upass:=c.Param("upass")
-//	c.String(http.StatusOK, fmt.Sprintf("%s", body)) //输出
-//}
-//
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
+	"starWebserver/Global"
+	"strconv"
+	"time"
+)
+
+func Login(c *gin.Context) {
+	name := c.Param("name")
+	upass := c.Param("upass")
+	sqlStr := "select uid from user where uname = ? and upass = ? limit 1"
+	Row := Global.DB.QueryRow(sqlStr, name, upass)
+	var uid string
+	err := Row.Scan(&uid)
+	if err != nil {
+		//todo:编写错误信息
+		fmt.Println(err)
+		c.Abort()
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "访问未授权"})
+		return
+	}
+	Now := time.Now().Format("2006-01-02 15:04:05")
+	session := strconv.FormatUint(rand.Uint64(), 16)
+	sqlStr = "insert into session (uid,session,lasttime) " +
+		"values ( ? , ? , ? ) " +
+		"on duplicate key update session= ? ,lasttime = ? ;"
+	_, err = Global.DB.Exec(sqlStr, uid, session, Now, session, Now)
+	if err != nil {
+		//todo:编写错误信息
+		fmt.Println(err)
+		c.Abort()
+		c.JSON(http.StatusUnauthorized, gin.H{"message": "访问未授权"})
+		return
+	}
+	c.String(http.StatusOK, fmt.Sprintf("%s", session)) //输出
+}
+
 //func Adduser(c *gin.Context) {
 //	name:=c.Param("name")
 //	upass:=c.Param("upass")
--- Routers/controller.go
+++ Routers/controller.go
@@ -9,18 +9,21 @@ import (
 func SetUpRouter(api *gin.Engine) {
 	setUpStar(api)
 	setUpUser(api)
+	setUpData(api)
 }
 
 func setUpStar(api *gin.Engine) {
 	api.GET("/constellation/getAll", Proxy.Log, Command.Star)
 }
 func setUpUser(api *gin.Engine) {
-	//api.GET("/user/login/:name/:upass",Command.Login)
+	api.GET("/user/login/:name/:upass", Command.Login)
 	//api.POST("/user/:name/upass/:uauth",Command.Adduser)
 	//api.POST("/user/:session/:name/:upass/:uauth",Command.AdminAdduser)
 	//api.PUT("/user/:session/:oldupass/:newupass",Command.Setuserpass)
 	//api.DELETE("/user/:session/:name/:upass",Command.Deluser)
 	//api.GET("/user/auth/:session",Command.Getuserauth)
+}
+func setUpData(api *gin.Engine) {
 	//api.GET("/data/querycount/:session",Command.Querycount)
 	api.GET("/data/starcount/:session", Proxy.SessionAuth, Proxy.SessionTimestamp, Command.Starcount)
 }
